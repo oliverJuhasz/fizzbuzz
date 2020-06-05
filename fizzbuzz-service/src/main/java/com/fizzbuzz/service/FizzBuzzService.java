81651ae01aa111450f781ecc95d4a25d356e745b
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-05 17:20:00 +0200

    Add FizzBuzzService code skeleton

---
+++ fizzbuzz-service/src/main/java/com/fizzbuzz/service/FizzBuzzService.java
@@ -0,0 +1,19 @@
+package com.fizzbuzz.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FizzBuzzService {
+
+
+    public List<String> generateFizzBuzzUpToNumber(long number) {
+        return null;
+    }
+
+    public String convertNumberToFizzBuzz(long number) {
+        return null;
+    }
+
+}
