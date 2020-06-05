96d62d608e86ede94cec350d5eaef7846dda9fcd
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-06 00:42:18 +0200

    Remove unused import

--- fizzbuzz-service/src/test/java/com/fizzbuzz/service/FizzBuzzServiceIntegrationTest.java
+++ fizzbuzz-service/src/test/java/com/fizzbuzz/service/FizzBuzzServiceIntegrationTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FizzBuzzServiceIntegrationTest {
 
