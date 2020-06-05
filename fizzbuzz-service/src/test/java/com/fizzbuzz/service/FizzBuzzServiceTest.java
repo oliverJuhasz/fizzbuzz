7e60884f7b97fe998454bca634a0edf1f6a5efa3
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-05 18:55:01 +0200

    Write failing test for nullcheck

---
+++ fizzbuzz-service/src/test/java/com/fizzbuzz/service/FizzBuzzServiceTest.java
@@ -0,0 +1,32 @@
+package com.fizzbuzz.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+public class FizzBuzzServiceTest {
+
+    @InjectMocks
+    private FizzBuzzService underTest;
+
+    @BeforeEach
+    private void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("convertNumberToFizzBuzz shouldn't return null")
+    public void test1() {
+        // GIVEN
+        long input = 10L;
+
+        // WHEN
+        String result = underTest.convertNumberToFizzBuzz(input);
+
+        // THEN
+        Assertions.assertNotNull(result);
+    }
+}
