4e732aec3bb0d8937934823b433fc311dad950af
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-06 00:42:04 +0200

    Add FizzBuzzServiceIntegrationTest

---
+++ fizzbuzz-service/src/test/java/com/fizzbuzz/service/FizzBuzzServiceIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.fizzbuzz.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class FizzBuzzServiceIntegrationTest {
+
+    @InjectMocks
+    private FizzBuzzService underTest;
+
+    @BeforeEach
+    private void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("generateFizzBuzzUpToNumber should return correct output for input 15")
+    public void test1() {
+        // GIVEN
+        long input = 15L;
+        List<String> expected = new ArrayList<>();
+        expected.add("1");
+        expected.add("2");
+        expected.add("fizz");
+        expected.add("4");
+        expected.add("buzz");
+        expected.add("fizz");
+        expected.add("7");
+        expected.add("8");
+        expected.add("fizz");
+        expected.add("buzz");
+        expected.add("11");
+        expected.add("fizz");
+        expected.add("13");
+        expected.add("14");
+        expected.add("fizzbuzz");
+
+        // WHEN
+        List<String> result = underTest.generateFizzBuzzUpToNumber(input);
+
+        // THEN
+        assertEquals(expected, result);
+    }
+
+}
