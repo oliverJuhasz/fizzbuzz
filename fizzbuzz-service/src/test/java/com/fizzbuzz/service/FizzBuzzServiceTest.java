f4775707bf1e9123fe6166f8d7aabfa20e58d492
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-05 23:33:30 +0200

    add test case ""convertNumberToFizzBuzz should return the number if it is not devisable by three or five"

--- fizzbuzz-service/src/test/java/com/fizzbuzz/service/FizzBuzzServiceTest.java
+++ fizzbuzz-service/src/test/java/com/fizzbuzz/service/FizzBuzzServiceTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzServiceTest {
 
@@ -48,5 +53,18 @@ public class FizzBuzzServiceTest {
 
         // THEN
         Assertions.assertThrows(IllegalArgumentException.class, () -> underTest.convertNumberToFizzBuzz(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("convertNumberToFizzBuzz should return the number if it is not devisable by three or five")
+    @ValueSource(longs = {1, 2, 4, 7, 8, 11, 13})
+    public void test4(long input) {
+        // GIVEN as parameter
+
+        // WHEN
+        String result = underTest.convertNumberToFizzBuzz(input);
+
+        // THEN
+        assertEquals(String.valueOf(input), result);
     }
 }
