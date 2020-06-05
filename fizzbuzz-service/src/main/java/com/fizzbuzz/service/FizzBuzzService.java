2e7acd417c5921d94e721bca24929d21e659c5c0
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-05 23:43:06 +0200

    Make test pass

--- fizzbuzz-service/src/main/java/com/fizzbuzz/service/FizzBuzzService.java
+++ fizzbuzz-service/src/main/java/com/fizzbuzz/service/FizzBuzzService.java
@@ -18,6 +18,9 @@ public class FizzBuzzService {
         }
         if (number % 3 == 0) {
             return "fizz";
+        }
+        else if (number % 5 == 0) {
+            return "buzz";
         } else {
             return String.valueOf(number);
         }
