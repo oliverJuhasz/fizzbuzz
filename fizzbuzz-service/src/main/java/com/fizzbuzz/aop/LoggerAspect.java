cd0bb62120e59c02487195c040be72f7ac1621dc
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-06 13:45:59 +0200

    Add logger to service

---
+++ fizzbuzz-service/src/main/java/com/fizzbuzz/aop/LoggerAspect.java
@@ -0,0 +1,46 @@
+package com.fizzbuzz.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+@Aspect
+public class LoggerAspect {
+
+
+    @Pointcut("within(com.fizzbuzz.service..*)")
+    protected void methodsInService() {}
+
+    @Around("methodsInService()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        long startTime = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long stopTotalTime = System.currentTimeMillis() - startTime;
+        StringBuilder stringBuilder = new StringBuilder("Parameter passed: ");
+        if (args != null && args.length > 0) {
+            for (Object arg : args) {
+                stringBuilder.append(arg.toString());
+            }
+        } else {
+            stringBuilder.append("none");
+        }
+        stringBuilder.append("\nReturn value: ").append(returnValue == null ? "No return value" : returnValue).append("\nExecution time: ")
+                .append(stopTotalTime);
+        log.trace(stringBuilder.toString());
+        return returnValue;
+    }
+    
+    @AfterThrowing(pointcut="within(com.fizzbuzz.service..*)", throwing="ex")
+    public void logException(Exception ex) {
+        log.error("Exception occured at" + LocalDateTime.now().toString() +": " + ex);
+    }
+}
