3534bd80d30d29fe76c37ea0b540dcd9a0ada30f
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-06 16:05:07 +0200

    Add WebApplicationBoot

---
+++ fizzbuzz-web/src/main/java/com/fizzbuzz/WebApplicationBoot.java
@@ -0,0 +1,14 @@
+package com.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationBoot {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationBoot.class, args);
+    }
+
+}
