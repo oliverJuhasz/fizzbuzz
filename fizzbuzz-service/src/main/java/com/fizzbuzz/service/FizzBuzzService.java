bd5808991b0ca1f16bcced8271a8f48263dcb2c1
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-07 00:25:14 +0200

    Add javadoc to service

--- fizzbuzz-service/src/main/java/com/fizzbuzz/service/FizzBuzzService.java
+++ fizzbuzz-service/src/main/java/com/fizzbuzz/service/FizzBuzzService.java
@@ -8,7 +8,11 @@ import java.util.List;
 @Service
 public class FizzBuzzService {
 
-
+    /** Takes a number and generates fizzbuzz sequence up to that number
+     *
+     * @param number the last number in the sequence
+     * @return List of fizzbuzz results
+     */
     public List<String> generateFizzBuzzUpToNumber(long number) {
         if (number < 0) {
             throw new IllegalArgumentException("Input shouldn't be less than zero");
@@ -20,6 +24,12 @@ public class FizzBuzzService {
         return fizzBuzzes;
     }
 
+    /** Takes a number and converts it to the fizzbuzz equivalent
+     *
+     * @param number to be converted
+     * @return fizz if number is divisable by 3, buzz if number is dividable by 5, and fizzbuzz if dividable by both.
+     * Returns the number if none of the above conditions apply.
+     */
     public String convertNumberToFizzBuzz(long number) {
         if (number <= 0) {
             throw new IllegalArgumentException("Input shouldn't be less than or equal to zero");
